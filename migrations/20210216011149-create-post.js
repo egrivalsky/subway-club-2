e59336b6f86f67ac0aabd7b0c5b0c8e89fa0f5a3
unknown <erik@computer.com>
2021-02-17 16:15:44 -0700

    changed userId datatype from string to integer

--- migrations/20210216011149-create-post.js
+++ migrations/20210216011149-create-post.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       userId: {
-        type: Sequelize.STRING
+        type: Sequelize.INTEGER
       },
       stationId: {
         type: Sequelize.STRING
