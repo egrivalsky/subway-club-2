6066207f1223ce87bdd282e49dd7e0104d876d54
unknown <erik@computer.com>
2021-04-23 12:43:06 -0600

    deployment attempt 2

--- models/index.js
+++ models/index.js
@@ -5,7 +5,15 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+
+let config;
+if (env === 'development') {
+//development:
+config = require(__dirname + '/../config/config.json')[env];
+} else {
+//production:
+const config = require(__dirname + '/../config/production.json')[env];
+}
 const db = {};
 
 let sequelize;
