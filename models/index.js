013fd90e3f982318c15e14b20d3b68957ebbc6e4
unknown <erik@computer.com>
2021-06-14 15:03:25 -0400

    changes settings.json and and paths in config configuration

--- models/index.js
+++ models/index.js
@@ -9,19 +9,21 @@ const env = process.env.NODE_ENV || 'development';
 let config;
 if (env === 'development') {
 //development:
-config = require(__dirname + '/../config/config.json')[env];
+// config = require(__dirname + '/../config/config.json')[env];
+config = require(__dirname + '/config/config.json')[env];
 } else {
 //production:
-const config = require(__dirname + '/../config/production.json')[env];
+// const config = require(__dirname + '/../config/production.json')[env];
+const config = require(__dirname + '/config/config.json')[env];
 }
 const db = {};
 
 let sequelize;
-// if (config.use_env_variable) {
+if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
-// } else {
-//   sequelize = new Sequelize(config.database, config.username, config.password, config);
-// }
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
